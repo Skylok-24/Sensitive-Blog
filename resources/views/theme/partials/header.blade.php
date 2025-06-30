adc584a710bf1f9e1314196fa7797c038f272a6c
Brahmia Lokman Abdelmoname <brahmia.lokmeneabdelmoname@univ-guelma.dz>
2025-06-30 21:48:23 +0100

    finish categories page

--- resources/views/theme/partials/header.blade.php
+++ resources/views/theme/partials/header.blade.php
@@ -1,4 +1,11 @@
+@php
+  use App\Models\Category;
 
+  $headerCategories = Category::take(3)->get();
+
+@endphp
+  
+  
   <!--================Header Menu Area =================-->
   <header class="header_area">
     <div class="main_menu">
@@ -18,11 +25,14 @@
               <li class="nav-item submenu dropdown @yield('category-active')">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                   aria-expanded="false">Categories</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item  @yield('category-active')  "><a class="nav-link" href="{{ route('theme.category') }}">Food</a></li>
-                  <li class="nav-item  @yield('category-active')  "><a class="nav-link" href="{{ route('theme.category') }}">Bussiness</a></li>
-                  <li class="nav-item  @yield('category-active') "><a class="nav-link" href="{{ route('theme.category') }}">Travel</a></li>
-                </ul>
+                  @if(count($headerCategories) > 0)
+                    <ul class="dropdown-menu">
+                      @foreach ($headerCategories as $category)
+                      
+                      <li class="nav-item  @yield('category-active')  "><a class="nav-link" href="{{ route('theme.category') }}">{{ $category->name }}</a></li>
+                  @endforeach
+                    </ul>
+                @endif
               </li>
               <li class="nav-item  @yield('contact-active') "><a class="nav-link" href="{{ route('theme.contact') }}">Contact</a></li>
             </ul>
--- resources/views/theme/partials/sidebar.blade.php
+++ resources/views/theme/partials/sidebar.blade.php
@@ -1,3 +1,12 @@
+@php
+  use App\Models\Category;
+
+  $categories = Category::get();
+
+@endphp
+
+
+
 <!-- Start Blog Post Siddebar -->
 <div class="col-lg-4 sidebar-widgets">
     <div class="widget-wrap">
@@ -26,38 +35,18 @@
 
         <div class="single-sidebar-widget post-category-widget">
             <h4 class="single-sidebar-widget__title">Catgory</h4>
+            @if(count($categories))
             <ul class="cat-list mt-20">
+                @foreach($categories as $category)
                 <li>
                     <a href="#" class="d-flex justify-content-between">
-                        <p>Technology</p>
+                        <p>{{ $category->name }}</p>
                         <p>(03)</p>
                     </a>
                 </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Software</p>
-                        <p>(09)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Lifestyle</p>
-                        <p>(12)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Shopping</p>
-                        <p>(02)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Food</p>
-                        <p>(10)</p>
-                    </a>
-                </li>
+                @endforeach
             </ul>
+            @endif
         </div>
 
         <div class="single-sidebar-widget popular-post-widget">
